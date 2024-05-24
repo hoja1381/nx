475a10c8cb3ac83fa09c310e36ec3d6f3de10030
hoja1381 <h.jahandideh2@gmail.com>
2024-05-24 16:15:30 +0330

    feat : init

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from '@nx-test/database';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
